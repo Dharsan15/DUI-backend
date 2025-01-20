2e3410ed1604bd11a3e0cbc2c4e28d8bfd636885
Dharsan15 <dharsanravi6677@gmail.com>
2025-01-20 23:20:39 +0530

    changed imports

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,20 @@ import express from "express";
 import mongoose from "mongoose"
 import { userModel } from "./models/user.js";
 import userRoutes from "./routes/users.js"
+import designtocodeRoutes from "./routes/designtocode.js"
+import cookieParser from "cookie-parser"
 import "dotenv/config";
 import cors from 'cors';
 
 
 const app = express();
 
-
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:5173",
-      credentials: true, 
+      origin: "https://ui-component-library-phi.vercel.app",
+      //credentials: true, 
     })
   );
 
@@ -25,43 +26,10 @@ mongoose.connection.on("open" , () => {
 })
 
 app.use("/api/users" , userRoutes);
+app.use("/api/designtocode" , designtocodeRoutes);
 
 
-// app.post(("/signup") , async (req , res) => {
-
-//      try {
-
-          
-//           const newuser = new userModel({
-//              username : req.body.username,
-//              email : req.body.email,
-//              accountType : req.body.accountType,
-//              password  : req.body.password
-//           })
-
-//          await newuser.save();
-//          console.log("saved user" , newuser);
-//          res.json({
-//             message : "user saved"
-//          })
-         
-//      }
-
-//      catch(err)
-//      {
-//          console.log(err);
-         
-//      }
-// })
-
-// app.post("/api/login" , async(req , res) => {
-     
-// })
-
-
-
-
-app.listen(8000 , () =>  {
-       console.log("server listening to the port 8000");    
+app.listen(3000 , () =>  {
+       console.log("server listening to the port 3000");    
 });
 
