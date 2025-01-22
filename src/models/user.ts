56a0bab458c83a278a50793f66a64bbf34005737
Dharsan15 <dharsanravi6677@gmail.com>
2025-01-22 21:53:28 +0530

    added models

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Model } from "mongoose";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 const accounttypes = ["Student" , "Professional" , "Organization"];
 
